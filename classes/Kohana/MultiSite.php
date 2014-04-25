be09bf2ea82e33f1e07ea1d73fe59a613ed71081
Spotman <i.am@spotman.ru>
2014-04-25 19:48:53 +0400

    domain detection

--- classes/Kohana/MultiSite.php
+++ classes/Kohana/MultiSite.php
@@ -111,7 +111,7 @@ abstract class Kohana_MultiSite {
         if ( ! static::$_domain )
         {
             // Attempt to use HTTP_HOST and fallback to SERVER_NAME
-            static::$_domain = getenv('http_host') OR getenv('server_name');
+            static::$_domain = getenv('http_host') ?: getenv('server_name');
 
             if ( ! static::$_domain  )
                 throw new HTTP_Exception_400('Can not determine domain name, possible bad request');
