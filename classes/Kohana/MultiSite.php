4eff158c7626d4ba4dadf1961772be284709a061
Spotman <i.am@spotman.ru>
2014-04-28 20:26:24 +0400

    clear static files cache minion task

--- classes/Kohana/MultiSite.php
+++ classes/Kohana/MultiSite.php
@@ -39,31 +39,41 @@ abstract class Kohana_MultiSite {
     }
 
     /**
-     * Performs search for per-site directory derived to current request domain and adds it to CFS
+     * Performs search for per-site directory and adds it to CFS
      *
      * @return $this
+     * @throws Kohana_Exception
      */
     public function process()
     {
-        // Getting current domain
-        $domain = $this->domain();
+        // Base script directory
+        $doc_root = Kohana::doc_root();
 
         $sites_path = realpath(static::config('path'));
 
-        // Getting per-site directory name for current domain
-        $site_name = $this->search($sites_path, $domain);
+        if ( strpos($doc_root, $sites_path) === FALSE )
+            throw new Kohana_Exception('Request must be initiated from per-site directory');
 
-        // If no config for current site, exit
-        if ( ! $site_name )
-        {
-            $this->missing_domain($domain);
-            return $this;
-        }
+        // Getting site name
+        $site_name = dirname(str_replace($sites_path.DIRECTORY_SEPARATOR, '', $doc_root));
+
+//        // Getting current domain
+//        $domain = $this->domain();
+//
+//        // Getting per-site directory name for current domain
+//        $site_name = $this->search($sites_path, $domain);
+//
+//        // If no config for current site, exit
+//        if ( ! $site_name )
+//        {
+//            $this->missing_domain($domain);
+//            return $this;
+//        }
 
         $site_path = realpath($sites_path.DIRECTORY_SEPARATOR.$site_name);
 
         // Saving per-site dir for later use
-        $this->site_path($site_path);
+        static::$_site_dir = $site_path;
 
         // Connecting per-site directory to CFS so it becomes top level path (which overrides /application/ path)
         Kohana::prepend_path($site_path);
@@ -88,37 +98,31 @@ abstract class Kohana_MultiSite {
     /**
      * Getter/setter for current per-site directory
      *
-     * @param string|null $path
      * @return string
      */
-    public function site_path($path = NULL)
+    public function site_path()
     {
-        if ( $path )
-        {
-            static::$_site_dir = $path;
-        }
-
         return static::$_site_dir;
     }
 
-    /**
-     * Returns the current request domain name
-     * @return string
-     * @throws HTTP_Exception_400
-     */
-    public function domain()
-    {
-        if ( ! static::$_domain )
-        {
-            // Attempt to use HTTP_HOST and fallback to SERVER_NAME
-            static::$_domain = getenv('HTTP_HOST') ?: getenv('SERVER_NAME');
-
-            if ( ! static::$_domain  )
-                throw new HTTP_Exception_400('Can not determine domain name, possible bad request');
-        }
-
-        return static::$_domain;
-    }
+//    /**
+//     * Returns the current request domain name
+//     * @return string
+//     * @throws HTTP_Exception_400
+//     */
+//    public function domain()
+//    {
+//        if ( ! static::$_domain )
+//        {
+//            // Attempt to use HTTP_HOST and fallback to SERVER_NAME
+//            static::$_domain = getenv('HTTP_HOST') ?: getenv('SERVER_NAME');
+//
+//            if ( ! static::$_domain  )
+//                throw new HTTP_Exception_400('Can not determine domain name, possible bad request');
+//        }
+//
+//        return static::$_domain;
+//    }
 
     /**
      * Restrict direct *new* call
@@ -153,109 +157,109 @@ abstract class Kohana_MultiSite {
         return $key ? self::$_config->get($key) : self::$_config;
     }
 
-    /**
-     * Performs search for per-site directory for provided domain
-     *
-     * @param $base_path string
-     * @param $domain string
-     * @return string|null
-     */
-    protected function search($base_path, $domain)
-    {
-        $directories = $this->get_subdirectories($base_path);
-
-        if ( ! $directories )
-        {
-            $this->missing_paths($base_path);
-            return NULL;
-        }
-
-        foreach ( $directories as $site_name )
-        {
-            $config_file = $base_path.DIRECTORY_SEPARATOR.$site_name.DIRECTORY_SEPARATOR.'config.php';
-
-            if ( file_exists($config_file) )
-            {
-                // Route by wildcards from per-site config
-                $wildcards = Kohana::load($config_file);
-            }
-            else
-            {
-                // Route directory name
-                $wildcards = array($site_name);
-            }
-
-            // Search for matching domain
-            foreach ( $wildcards as $url )
-            {
-                if ( $this->check($domain, $url) )
-                    return $site_name;
-            }
-        }
-
-        return NULL;
-    }
-
-    /**
-     * Returns list of per-site directories
-     *
-     * @param $base_path
-     * @return array
-     * @throws HTTP_Exception_500
-     */
-    protected function get_subdirectories($base_path)
-    {
-        $paths = array();
-
-        /** @var DirectoryIterator|DirectoryIterator[] $dir */
-        $dir = new DirectoryIterator($base_path);
-
-        if ( ! $dir->isDir() )
-            throw new HTTP_Exception_500('The path is not a directory: :path', array(':path' => Debug::path($base_path)));
-
-        foreach ($dir as $item)
-        {
-            if ( ! $item->isDir() OR $item->isDot() )
-                continue;
-
-            $paths[] = $item->getFilename();
-        }
-
-        return $paths;
-    }
-
-    /**
-     * Checks the domain for provided wildcard
-     *
-     * @param $domain string
-     * @param $wildcard string Wildcard like "*.example.com"
-     * @return bool
-     */
-    protected function check($domain, $wildcard)
-    {
-        return fnmatch($wildcard, $domain);
-    }
-
-    /**
-     * Behavior for unknown domain
-     *
-     * @param string $domain Requested domain
-     * @throws HTTP_Exception_404
-     */
-    protected function missing_domain($domain)
-    {
-        throw new HTTP_Exception_404('Unknown domain name requested: :domain', array(':domain' => $domain));
-    }
-
-    /**
-     * Behavior for empty base directory
-     *
-     * @param string $base_path Base directory path
-     * @throws HTTP_Exception_500
-     */
-    protected function missing_paths($base_path)
-    {
-        throw new HTTP_Exception_500('No paths found for MultiSite module in :path;', array(':path' => $base_path));
-    }
+//    /**
+//     * Performs search for per-site directory for provided domain
+//     *
+//     * @param $base_path string
+//     * @param $domain string
+//     * @return string|null
+//     */
+//    protected function search($base_path, $domain)
+//    {
+//        $directories = $this->get_subdirectories($base_path);
+//
+//        if ( ! $directories )
+//        {
+//            $this->missing_paths($base_path);
+//            return NULL;
+//        }
+//
+//        foreach ( $directories as $site_name )
+//        {
+//            $config_file = $base_path.DIRECTORY_SEPARATOR.$site_name.DIRECTORY_SEPARATOR.'config.php';
+//
+//            if ( file_exists($config_file) )
+//            {
+//                // Route by wildcards from per-site config
+//                $wildcards = Kohana::load($config_file);
+//            }
+//            else
+//            {
+//                // Route directory name
+//                $wildcards = array($site_name);
+//            }
+//
+//            // Search for matching domain
+//            foreach ( $wildcards as $url )
+//            {
+//                if ( $this->check($domain, $url) )
+//                    return $site_name;
+//            }
+//        }
+//
+//        return NULL;
+//    }
+//
+//    /**
+//     * Returns list of per-site directories
+//     *
+//     * @param $base_path
+//     * @return array
+//     * @throws HTTP_Exception_500
+//     */
+//    protected function get_subdirectories($base_path)
+//    {
+//        $paths = array();
+//
+//        /** @var DirectoryIterator|DirectoryIterator[] $dir */
+//        $dir = new DirectoryIterator($base_path);
+//
+//        if ( ! $dir->isDir() )
+//            throw new HTTP_Exception_500('The path is not a directory: :path', array(':path' => Debug::path($base_path)));
+//
+//        foreach ($dir as $item)
+//        {
+//            if ( ! $item->isDir() OR $item->isDot() )
+//                continue;
+//
+//            $paths[] = $item->getFilename();
+//        }
+//
+//        return $paths;
+//    }
+//
+//    /**
+//     * Checks the domain for provided wildcard
+//     *
+//     * @param $domain string
+//     * @param $wildcard string Wildcard like "*.example.com"
+//     * @return bool
+//     */
+//    protected function check($domain, $wildcard)
+//    {
+//        return fnmatch($wildcard, $domain);
+//    }
+//
+//    /**
+//     * Behavior for unknown domain
+//     *
+//     * @param string $domain Requested domain
+//     * @throws HTTP_Exception_404
+//     */
+//    protected function missing_domain($domain)
+//    {
+//        throw new HTTP_Exception_404('Unknown domain name requested: :domain', array(':domain' => $domain));
+//    }
+//
+//    /**
+//     * Behavior for empty base directory
+//     *
+//     * @param string $base_path Base directory path
+//     * @throws HTTP_Exception_500
+//     */
+//    protected function missing_paths($base_path)
+//    {
+//        throw new HTTP_Exception_500('No paths found for MultiSite module in :path;', array(':path' => $base_path));
+//    }
 
 }
