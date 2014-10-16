03d94e4f110c439d7a5c5a2095ca1475fdc00ed2
Spotman <i.am@spotman.ru>
2014-10-16 17:38:42 +0400

    error reporting fix

--- init.php
+++ init.php
@@ -1,3 +1,7 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
 MultiSite::instance()->process();
+
+Kohana::$log->attach(
+    new Log_File(MultiSite::instance()->site_path().DIRECTORY_SEPARATOR.'logs')
+);
